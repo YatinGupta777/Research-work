0e54007cb220a60b87050524dec862fcbaf47a38
Yatin Gupta <yatingupta7777@gmail.com>
2018-02-14 11:38:55 +0530

    Everything Fixed

--- Research/sa.py
+++ Research/sa.py
@@ -88,7 +88,7 @@ def get_all_hash(hashtag):
                                 # Just exit if any error
                     #        print("some error : " + str(e))
                      #       break
-                    z=open(hashtag+'.txt','w')
+                z=open(hashtag+'.txt','w')
                 for tweet in list_tweets:
                     tweet=tweet.encode('ascii','ignore')
                     z.write(tweet.decode())
@@ -184,7 +184,7 @@ def score(name):
     ans.append(0)
     ans.append(0)
     special_word=None
-    special_score=None ###CHANGED FROM NONE
+    special_score= 0 ###CHANGED FROM NONE
 
         #r=open(sys.argv[1],'r')
     for line in name:
@@ -192,133 +192,134 @@ def score(name):
         line=line.replace("."," ")
         line=line.split(" ")
         list_words=line
-    #    print(list_words[0])
+        #print(list_words[0])
 
-    for word in list_words:
-        with open(filename,encoding="ISO-8859-1",newline='') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                #print(row)
-                my_list = row
-                word = word.lower()
-                if (word == my_list[0] and len(word)>=1):
-                    ans_list=[]
-                    count_adj+=1
-                    #print(count)
-                    ans_list.append(word)
-                    ans_list.append(my_list[1])
-                    ans_list.append(my_list[3])
-                    ans_list.append(my_list[5])
-                    ans_list.append(my_list[7])
-                    ans_list.append(my_list[9])
+        for word in list_words:
+            with open(filename,encoding="ISO-8859-1",newline='') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    #print(row)
+                    my_list = row
+                    word = word.lower()
+                    if (word == my_list[0] and len(word)>=1):
+                        ans_list=[]
+                        count_adj+=1
+                        #print(count)
+                        ans_list.append(word)
+                        ans_list.append(my_list[1])
+                        ans_list.append(my_list[3])
+                        ans_list.append(my_list[5])
+                        ans_list.append(my_list[7])
+                        ans_list.append(my_list[9])
 
 
-        #FIX THISSS
+            #FIX THISSS
 
-                if special_word is None:
-                    ans[0]+=float(my_list[1])
-                    ans[1]+=float(my_list[3])
-                    ans[2]+=float(my_list[5])
-                    ans[3]+=float(my_list[7])
-                    ans[4]+=float(my_list[9])
-                else:
-                    print(special_word,word)
-                    print(special_score)
-                if special_score>=0:
-                    ans[0]+=float(my_list[1])*max(0.5,float(special_score))
-                    ans[1]+=float(my_list[3])*max(0.5,float(special_score))
-                    ans[2]+=float(my_list[5])*max(0.5,float(special_score))
-                    ans[3]+=float(my_list[7])*max(0.5,float(special_score))
-                    ans[4]+=float(my_list[9])*max(0.5,float(special_score))
-                else:
-                    ans[0]+=5-float(my_list[1])
-                    ans[1]+=5-float(my_list[3])
-                    ans[2]+=5-float(my_list[5])
-                    ans[3]+=5-float(my_list[7])
-                    ans[4]+=5-float(my_list[9])
-                    special_word=None
-                            #final_list.append(ans_list)
-                            #print(ans_list)
-        break
+                        if special_word is None:
+                            ans[0]+=float(my_list[1])
+                            ans[1]+=float(my_list[3])
+                            ans[2]+=float(my_list[5])
+                            ans[3]+=float(my_list[7])
+                            ans[4]+=float(my_list[9])
+                        else:
+                            #print(special_word,word)
+                            #print(special_score)
+                            if special_score >= 0:
+                                ans[0]+=float(my_list[1])*max(0.5,float(special_score))
+                                ans[1]+=float(my_list[3])*max(0.5,float(special_score))
+                                ans[2]+=float(my_list[5])*max(0.5,float(special_score))
+                                ans[3]+=float(my_list[7])*max(0.5,float(special_score))
+                                ans[4]+=float(my_list[9])*max(0.5,float(special_score))
+                            else:
+                                ans[0]+=5-float(my_list[1])
+                                ans[1]+=5-float(my_list[3])
+                                ans[2]+=5-float(my_list[5])
+                                ans[3]+=5-float(my_list[7])
+                                ans[4]+=5-float(my_list[9])
+                                special_word=None
+                                #final_list.append(ans_list)
+                                #print(ans_list)
+            #break
 
-    #FILENAME CHANGED FOR DEBUGGING ORIGINAL == 'adverb.csv'
+        #FILENAME CHANGED FOR DEBUGGING ORIGINAL == 'adverb.csv'
 
-        with open('adverb.csv',encoding="ISO-8859-1",newline='') as a:
-            reader=csv.reader(a)
-            for row in reader:
-                list_adverb=row
-                            #print(list_adverb)
-                word=word.lower()
-                if (word == list_adverb[0]):
-                    count+=1
-                    ans_list=[]
-                    ans_list.append(word)
-                    ans_list.append(list_adverb[1])
-                    ans_list.append(list_adverb[1])
-                    ans_list.append(list_adverb[1])
-                    ans_list.append(list_adverb[1])
-                    ans_list.append(list_adverb[1])
-                                #ans[0]+=float(list_adverb[1])
-                                #ans[1]+=float(list_adverb[1])
-                                #ans[2]+=float(list_adverb[1])
-                                #ans[3]+=float(list_adverb[1])
-                                #ans[4]+=float(list_adverb[1])
+            with open('adverb.csv',encoding="ISO-8859-1",newline='') as a:
+                reader=csv.reader(a)
+                for row in reader:
+                    list_adverb=row
+                                #print(list_adverb)
+                    word=word.lower()
+                    if (word == list_adverb[0]):
+                        count+=1
+                        ans_list=[]
+                        ans_list.append(word)
+                        ans_list.append(list_adverb[1])
+                        ans_list.append(list_adverb[1])
+                        ans_list.append(list_adverb[1])
+                        ans_list.append(list_adverb[1])
+                        ans_list.append(list_adverb[1])
+                                    #ans[0]+=float(list_adverb[1])
+                                    #ans[1]+=float(list_adverb[1])
+                                    #ans[2]+=float(list_adverb[1])
+                                    #ans[3]+=float(list_adverb[1])
+                                    #ans[4]+=float(list_adverb[1])
 
-                #FIX THISS BELOWW
+                    #FIX THISS BELOWW
 
-                if special_word is None:
-                    special_word=word
-                    special_score=float(list_adverb[1])
-                else:
-                    special_word=word
-                    special_score=float(special_score)*float(list_adverb[1])
+                        if special_word is None:
+                            special_word=word
+                            special_score=float(list_adverb[1])
+                        else:
+                            special_word=word
+                            special_score=float(special_score)*float(list_adverb[1])
+
+                                            #print(ans_list)
+                    #break
+
+
+            #FILENAME CHANGED FOR DEBUGGING ORIGINAL == 'verb.csv'
+
+            with open('verb.csv',encoding="ISO-8859-1",newline='') as v:
+                reader=csv.reader(v)
+                for row in reader:
+                    list_verb=row
+                    word=word.lower()
+                                #print(list_adverb)
+                    if (word==list_verb[0]):
+                        count+=1
+
+                    #FIX THIS BELOW
+
+                        ans_list=[]
+                        ans_list.append(word)
+                        ans_list.append(list_verb[1])
+                        ans_list.append(list_verb[1])
+                        ans_list.append(list_verb[1])
+                        ans_list.append(list_verb[1])
+                        ans_list.append(list_verb[1])
+
+                    #FIX THIS BELOW
+
+
+                        if special_word is None:
+                            special_word=word
+                            special_score=float(list_verb[1])
+                        else:
+                            special_word=word
+                            special_score=float(special_score)*float(list_verb[1])
 
                                         #print(ans_list)
-                break
-
-
-        #FILENAME CHANGED FOR DEBUGGING ORIGINAL == 'verb.csv'
-
-        with open('verb.csv',encoding="ISO-8859-1",newline='') as v:
-            reader=csv.reader(v)
-            for row in reader:
-                list_verb=row
-                word=word.lower()
-                            #print(list_adverb)
-                if (word==list_verb[0]):
-                    count+=1
-
-                #FIX THIS BELOW
-
-                ans_list=[]
-                ans_list.append(word)
-                ans_list.append(list_verb[1])
-                ans_list.append(list_verb[1])
-                ans_list.append(list_verb[1])
-                ans_list.append(list_verb[1])
-                ans_list.append(list_verb[1])
-
-                #FIX THIS BELOW
-
-
-                if special_word is None:
-                    special_word=word
-                    special_score=float(list_verb[1])
-                else:
-                    special_word=word
-                    special_score=float(special_score)*float(list_verb[1])
-
-                                        #print(ans_list)
-                break
+                #break
 
 
             #print(count)
             #print(ans)
-        for i in range(0,5):
+    for i in range(0,5):
                 #ans[i]=ans[i]/((5*count_adj)+count)
-            if count_adj != 0:
-                ans[i]=ans[i]/(5*count_adj)
-    print(ans)
+        if count_adj != 0:
+            ans[i]=ans[i]/(5*count_adj)
+
+    #print(ans)
     return ans
 
 
